e15f5b59901e0867eda59572492f4c30cec5b144
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-05-29 23:41:17 +0530

    chore: Remove redundant variable declaration

--- frappe/core/doctype/user/user.js
+++ frappe/core/doctype/user/user.js
@@ -439,8 +439,6 @@ function get_roles_for_editing_user() {
 }
 
 function show_api_key_dialog(api_key, api_secret) {
-	const download_icon = `<i class="fa fa-download fa-w"></i>`;
-
 	const dialog = new frappe.ui.Dialog({
 		title: __("Key Details"),
 		fields: [
@@ -461,7 +459,7 @@ function show_api_key_dialog(api_key, api_secret) {
 		],
 		static: true,
 		size: "small",
-		primary_action_label: __("{0} Download", [download_icon]),
+		primary_action_label: __("{0} Download", [`<i class="fa fa-download fa-w"></i>`]),
 		primary_action: function () {
 			frappe.tools.downloadify(
 				[
