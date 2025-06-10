9de31d8516479689d5f1cacc84464d13607cb2b5
Ankush Menat <ankush@frappe.io>
2025-06-10 09:46:46 +0530

    ci: Don't run CI with PYTHONOPTIMIZE flag
    :facepalm:

--- frappe/core/doctype/rq_job/test_rq_job.py
+++ frappe/core/doctype/rq_job/test_rq_job.py
@@ -175,7 +175,7 @@ class TestRQJob(IntegrationTestCase):
 		# If this starts failing analyze memory usage using memray or some equivalent tool to find
 		# offending imports/function calls.
 		# Refer this PR: https://github.com/frappe/frappe/pull/21467
-		LAST_MEASURED_USAGE = 42
+		LAST_MEASURED_USAGE = 46
 		if frappe.conf.use_mysqlclient:
 			# TEMP: Add extra allowance for running two connectors, this should be rolled back before v16
 			LAST_MEASURED_USAGE += 2
